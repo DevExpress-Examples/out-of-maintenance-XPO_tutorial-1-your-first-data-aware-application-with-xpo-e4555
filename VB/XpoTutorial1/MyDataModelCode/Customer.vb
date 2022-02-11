d4e9f21f7e5d4335c5fac79d73fcbf8b33d77967
DevExpressExampleBot <fakeEmail>
2022-02-11 14:21:17 +0300

    autogenerate VB

--- VB/XpoTutorial1/MyDataModelCode/Customer.vb
+++ VB/XpoTutorial1/MyDataModelCode/Customer.vb
@@ -1,15 +1,15 @@
-﻿Imports System
 Imports DevExpress.Xpo
-Imports DevExpress.Data.Filtering
+
 Namespace XpoTutorial1.DataModel
 
-	Partial Public Class Customer
-		Public Sub New(ByVal session As Session)
-			MyBase.New(session)
-		End Sub
-		Public Overrides Sub AfterConstruction()
-			MyBase.AfterConstruction()
-		End Sub
-	End Class
+    Public Partial Class Customer
 
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
+
+        Public Overrides Sub AfterConstruction()
+            MyBase.AfterConstruction()
+        End Sub
+    End Class
 End Namespace
